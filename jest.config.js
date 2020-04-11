0f051190918943eafddda20bb0f125c36655dfd2
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-04-12 01:10:48 +0300

    chore(app): config
    logger config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	roots: ['<rootDir>/tests'],
-	setupFiles: ['<rootDir>/tests/setup.ts'],
-	"collectCoverageFrom": [
-		"<rootDir>/src/**/*.ts",
-		"!**/node_modules/**",
-	]
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests", "<rootDir>/src/**/*.test.ts"],
+  setupFiles: ["<rootDir>/scripts/setup.ts"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!**/node_modules/**"],
+  moduleNameMapper: {
+    "^@config": "<rootDir>/src/config.ts",
+    "^@database/(.*)$": "<rootDir>/src/database/$1",
+    "^@core/(.*)$": "<rootDir>/src/core/$1",
+    "^@logger": "<rootDir>/src/core/logger.ts",
+    "^@routes/(.*)$": "<rootDir>/src/routes/$1",
+    "^@routesv1/(.*)$": "<rootDir>/src/routes/v1/$1",
+    "^@auth/(.*)$": "<rootDir>/src/auth/$1",
+  },
 };
