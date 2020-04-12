3c42ef19c59ce472ea431b7eae73da8998321917
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-04-12 22:46:37 +0300

    test(jest): test discovery

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["<rootDir>/src/**/*.test.ts"],
+  // roots: ["<rootDir>/src/**/*.test.ts"],
+  testMatch: [
+    "<rootDir>/__tests__/**/*.{js,ts,tsx,jsx,mjs}",
+    "<rootDir>/?(*.)(spec|test).{js,jsx,ts,tsx,mjs}",
+    "<rootDir>/src/**/__tests__/**/*.{js,ts,tsx,jsx,mjs}",
+    "<rootDir>/src/**/?(*.)(spec|test).{js,ts,tsx,jsx,mjs}",
+  ],
   setupFiles: ["<rootDir>/scripts/setup.ts"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!**/node_modules/**"],
   transform: {
-    "^.+\\.ts$": "ts-jest"
+    "^.+\\.ts$": "ts-jest",
   },
   moduleNameMapper: {
     "^@config": "<rootDir>/src/config.ts",
@@ -17,6 +23,6 @@ module.exports = {
     "^@routes/(.*)$": "<rootDir>/src/routes/$1",
     "^@routesv1/(.*)$": "<rootDir>/src/routes/v1/$1",
     "^@auth/(.*)$": "<rootDir>/src/auth/$1",
-    "^@utils/(.*)$": "<rootDir>/src/utils/$1"
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
   },
 };
