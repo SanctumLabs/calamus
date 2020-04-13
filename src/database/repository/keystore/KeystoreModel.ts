969b87ff8262a2443ae3222a99a18e491f3b2ded
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-04-13 09:44:37 +0300

    feat(database): repositories and models
    Repositories and models for management of users, roles, keystores and blogs

---
+++ src/database/repository/keystore/KeystoreModel.ts
@@ -0,0 +1,53 @@
+import { Schema, model, Document } from 'mongoose';
+import User from '@database/repository/user/UserModel';
+
+export const DOCUMENT_NAME = 'Keystore';
+export const COLLECTION_NAME = 'keystores';
+
+export default interface Keystore extends Document {
+	client: User;
+	primaryKey: string;
+	secondaryKey: string;
+	status?: boolean;
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+const schema = new Schema(
+	{
+		client: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+			index: true
+		},
+		primaryKey: {
+			type: Schema.Types.String,
+			required: true,
+			index: true
+		},
+		secondaryKey: {
+			type: Schema.Types.String,
+			required: true,
+			index: true
+		},
+		status: {
+			type: Schema.Types.Boolean,
+			default: true
+		},
+		createdAt: {
+			type: Date,
+			required: true,
+			select: false
+		},
+		updatedAt: {
+			type: Date,
+			required: true,
+			select: false
+		}
+	},
+	{
+		versionKey: false
+	});
+
+export const KeystoreModel = model<Keystore>(DOCUMENT_NAME, schema, COLLECTION_NAME);
