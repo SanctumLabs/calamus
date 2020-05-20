fc4b6ae34f441197186db443a16ffa15adf3825c
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-05-20 22:16:57 +0300

    feat(blog): latest api
    Fetch all the latest blogs

--- src/routes/blog/list/index.ts
+++ src/routes/blog/list/index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import express, { response } from 'express';
 import { SuccessResponse } from '@core/ApiResponse';
 import { NoDataError, BadRequestError } from '@core/ApiError';
-import blogReposiory from '@repository/blog';
+import blogRepository from '@repository/blog';
 import { Types } from 'mongoose';
 import validator, { ValidationSource } from '@utils/validators';
 import schema from './schema';
@@ -22,7 +22,7 @@ router.get(
     try {
       const tag = request.params.tag;
       const { pageNumber, pageItemCount } = request.query;
-      const blogs = await blogReposiory.findByTagAndPaginated(
+      const blogs = await blogRepository.findByTagAndPaginated(
         tag,
         // @ts-ignore
         parseInt(pageNumber),
@@ -50,7 +50,7 @@ router.get(
   validator(schema.authorId, ValidationSource.PARAM),
   asyncHandler(async (request, response) => {
     try {
-      const blogs = await blogReposiory.findAllPublishedForAuthor({
+      const blogs = await blogRepository.findAllPublishedForAuthor({
         id: new Types.ObjectId(request.params.id),
       } as User);
 
@@ -62,6 +62,31 @@ router.get(
     } catch (error) {
       logger.error(`Error getting blogs for author ${request.params.id}, ${error}`);
       response.status(500).send({ message: 'Error getting blogs for author' });
+    }
+  }),
+);
+
+/**
+ * Get all the latest blogs
+ */
+router.get(
+  '/latest',
+  validator(schema.pagination, ValidationSource.QUERY),
+  asyncHandler(async (req, res) => {
+    try {
+      const blogs = await blogRepository.findLatestBlogs(
+        // @ts-ignore
+        parseInt(req.query.pageNumber),
+        // @ts-ignore
+        parseInt(req.query.pageItemCount),
+      );
+
+      if (!blogs) throw new NoDataError();
+
+      return new SuccessResponse('success', blogs).send(res);
+    } catch (error) {
+      logger.error(`Failed to fetch latest blogs ${error}`);
+      response.status(500).send({ message: error.message });
     }
   }),
 );
