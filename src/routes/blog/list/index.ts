9df89b9335d486aa6988e35d4fb8bc44eb25ae07
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-05-20 22:39:40 +0300

    chore(lint): import
    remove unused import

--- src/routes/blog/list/index.ts
+++ src/routes/blog/list/index.ts
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express from 'express';
 import { SuccessResponse } from '@core/ApiResponse';
 import { NoDataError, BadRequestError } from '@core/ApiError';
 import blogRepository from '@repository/blog';
